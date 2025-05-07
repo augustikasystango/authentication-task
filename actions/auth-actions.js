5fcc17c5ed507b2e1b4f3d018e5f1a09bf82f714
Augustika Pal <augustika.pal@systango.com>
2025-05-07 17:04:55 +0530

    added login server action

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -1,7 +1,7 @@
 'use server';
-import { hashUserPassword } from "@/lib/hash";
-import { createUser } from "@/lib/user";
-import { createAuthSession } from "@/lib/auth";
+import { hashUserPassword, verifyPassword } from "@/lib/hash";
+import { createUser, getUserByEmail } from "@/lib/user";
+import { createAuthSession, verifyAuth } from "@/lib/auth";
 import { redirect } from "next/navigation";
 export async function signup( prevState,formData)
 {
@@ -29,6 +29,7 @@ export async function signup( prevState,formData)
     
     const id = createUser(email,hashedPassword  );
     await createAuthSession(id);
+    redirect('/training')
     }
    catch(error)
    {
@@ -44,5 +45,47 @@ export async function signup( prevState,formData)
    }
    redirect('/training');
 
+
+}
+
+export async function login(prevState,formData)
+{
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    const existingUser = getUserByEmail(email);
+
+    if(!existingUser)
+    {
+        return {
+            errors:{
+                email : 'Could not authenticate user , please check your credentials.'
+            }
+        }
+    }
+    const isValidPassword = verifyPassword(existingUser.password,password);
+
+    if(!isValidPassword)
+    {
+        return {
+            errors:{
+                email : 'Could not authenticate user , please check your credentials.'
+            }
+        }
+    }
+    await createAuthSession(existingUser.id);
+    redirect('/training')
+
+
+
+}
+
+export async function auth(mode,prevState,formData)
+{
+    if(mode === 'login')
+    {
+        return login(prevState,formData)
+    }
+    return signup(prevState,formData);
 
 }
